dc900a27710324f52e6bdb65a9e349d2e125520e
Debbie Christopher <debbie@tapsplace.com>
2018-11-09 11:29:34 -0600

    checking all saved items

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean, default=False)
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
